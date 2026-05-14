cf783ac93d479c354a0b92d1cbd1eab864559c84
macbook <graceolubiyi@gmail.com>
2026-05-14 11:46:02 +0100

    wip

--- src/main/java/com/bank/MultiTenantBankingApplication.java
+++ src/main/java/com/bank/MultiTenantBankingApplication.java
@@ -27,8 +27,6 @@ public class MultiTenantBankingApplication {
 		System.setProperty("POSTGRES_DB_USER", dotenv.get("POSTGRES_DB_USER"));
 		System.setProperty("POSTGRES_DB_PASSWORD", dotenv.get("POSTGRES_DB_PASSWORD"));
 		System.setProperty("POSTGRES_DB_PORT", dotenv.get("POSTGRES_DB_PORT"));
-		System.setProperty("JWT_PRIVATE_KEY_PATH", dotenv.get("JWT_PRIVATE_KEY_PATH"));
-		System.setProperty("JWT_PUBLIC_KEY_PATH", dotenv.get("JWT_PUBLIC_KEY_PATH"));
 		System.setProperty("JWT_ACCESS_TOKEN_EXPIRATION", dotenv.get("JWT_ACCESS_TOKEN_EXPIRATION"));
 		System.setProperty("JWT_REFRESH_TOKEN_EXPIRATION", dotenv.get("JWT_REFRESH_TOKEN_EXPIRATION"));
 
