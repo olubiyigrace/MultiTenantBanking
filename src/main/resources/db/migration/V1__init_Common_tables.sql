848ecb93600c5f11599292e6472da75d181369aa
macbook <graceolubiyi@gmail.com>
2026-05-17 01:52:55 +0100

    wip

--- src/main/resources/db/migration/V1__init_Common_tables.sql
+++ src/main/resources/db/migration/V1__init_Common_tables.sql
@@ -14,46 +14,54 @@ create table institutions
     email_verification_token_expiry timestamp(6),
     email_verified_at               timestamp(6),
     institution_email               varchar(255) not null
-            unique,
+        unique,
     institution_name                varchar(255) not null,
     institution_phone               varchar(255) not null,
     institution_rc_number           varchar(255) not null
-            unique,
+        unique,
     institution_status              varchar(255)
         constraint institutions_institution_status_check
             check ((institution_status)::text = ANY
-        ((ARRAY ['PENDING'::character varying, 'ACTIVE'::character varying, 'INACTIVE'::character varying, 'SUSPENDED'::character varying])::text[])),
+        (ARRAY [('PENDING'::character varying)::text, ('ACTIVE'::character varying)::text, ('INACTIVE'::character varying)::text, ('SUSPENDED'::character varying)::text])),
     institution_type                varchar(255)
         constraint institutions_institution_type_check
             check ((institution_type)::text = ANY
-                   ((ARRAY ['COOPERATIVE'::character varying, 'MICROFINANCE'::character varying])::text[])),
+                   (ARRAY [('COOPERATIVE'::character varying)::text, ('MICROFINANCE'::character varying)::text])),
     is_verified                     boolean
 );
 
-
 create table users
 (
-    id                varchar(255) not null
-        primary key,
+    id                varchar(255) not null primary key,
     created_at        timestamp(6) not null,
     deleted_at        timestamp(6),
-    email             varchar(255) not null
-            unique,
+    email             varchar(255) not null unique,
     enabled           boolean,
     name              varchar(255) not null,
     nin               varchar(255) not null,
     password          varchar(255) not null,
+    email_verification_token        varchar(255),
+    email_verification_token_expiry timestamp(6),
+    email_verified_at               timestamp(6),
     phone             varchar(255) not null,
+
     user_account_type varchar(255)
         constraint users_user_account_type_check
-            check ((user_account_type)::text = ANY
-        ((ARRAY ['SUPER_ADMIN'::character varying, 'INSTITUTION_ADMIN'::character varying, 'LOAN_OFFICER'::character varying, 'ACCOUNTANT'::character varying, 'MEMBER'::character varying])::text[])),
+            check (
+                (user_account_type)::text = ANY (
+        ARRAY[
+        ('SUPER_ADMIN'::character varying)::text,
+        ('INSTITUTION_ADMIN'::character varying)::text,
+        ('LOAN_OFFICER'::character varying)::text,
+        ('ACCOUNTANT'::character varying)::text,
+        ('MEMBER'::character varying)::text
+        ]
+    )),
     username          varchar(255) not null,
-    institution_id    varchar(255)
-        constraint fk_user_institution_id
-            references institutions
+    institution_id    varchar(255),
+    is_verified       boolean,
+
+    constraint fk_user_institution_id
+        foreign key (institution_id)
+        references institutions(id)
 );
-
-
-
-
