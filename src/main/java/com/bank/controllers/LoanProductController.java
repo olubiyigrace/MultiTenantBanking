13bbfc7397557fec2670bec32cab108390b55a56
macbook <graceolubiyi@gmail.com>
2026-05-22 15:51:56 +0100

    wip

--- src/main/java/com/bank/controllers/LoanProductController.java
+++ src/main/java/com/bank/controllers/LoanProductController.java
@@ -1,15 +1,25 @@
 package com.bank.controllers;
 
+import com.bank.requests.LoanProductRequest;
 import com.bank.services.LoanProductService;
+import com.bank.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/v1/")
 @PreAuthorize("hasRole('INSTITUTION_ADMIN')")
 public class LoanProductController {
     private final LoanProductService loanProductService;
+
+    @PostMapping("/create-product")
+    public ResponseEntity<ApiResponse<String>> create(LoanProductRequest loanProductRequest){
+        loanProductService.create(loanProductRequest);
+        return ResponseEntity.ok(ApiResponse.success(true, "Loan product created successfully", null));
+    }
 }
