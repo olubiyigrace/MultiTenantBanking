ac4d5f1d24141cda29d7776f2f623e1c2c7a5bb7
macbook <graceolubiyi@gmail.com>
2026-05-26 22:06:14 +0100

    wip

--- src/main/java/com/bank/controllers/AuthController.java
+++ src/main/java/com/bank/controllers/AuthController.java
@@ -6,7 +6,6 @@ import com.bank.auth.service.AuthenticationService;
 import com.bank.requests.RegisterInstitutionRequest;
 import com.bank.auth.requests.RegisterUserRequest;
 import com.bank.utils.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -14,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -22,38 +22,39 @@ public class AuthController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register-institution")
-    public ResponseEntity<ApiResponse<String>> registerInstitution(@Valid @RequestBody final RegisterInstitutionRequest registerInstitutionRequest) throws MessagingException {
+    public ResponseEntity<ApiResponse<String>> registerInstitution(
+            @Valid @RequestBody final RegisterInstitutionRequest registerInstitutionRequest)
+            throws MessagingException {
         authenticationService.registerInstitution(registerInstitutionRequest);
-        return ResponseEntity.ok(ApiResponse.success(true, "Almost there! Check your email to complete your registration.", null));
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Almost there! Check your email to complete your registration.", null));
     }
 
     @GetMapping("/verify-institution")
-    public ResponseEntity<ApiResponse<String>> verifyEmail(@RequestParam final String verificationTokenFromRequest, @RequestParam final String email) {
+    public ResponseEntity<ApiResponse<String>> verifyEmail(@RequestParam final String verificationTokenFromRequest,
+                                                           @RequestParam final String email) {
         authenticationService.verifyEmail(verificationTokenFromRequest, email);
-        return ResponseEntity.ok(ApiResponse.success(true, "Institution registered successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Institution registered successfully",
+                null));
     }
 
     @GetMapping("/reverify-institution")
     public ResponseEntity<ApiResponse<String>> resendUserVerificationEmail(@RequestParam final String email) {
         authenticationService.reverifyInstitutionEmail(email);
-        return ResponseEntity.ok(ApiResponse.success(true, "Resent! Check your email to complete your registration.", null));
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Resent! Check your email to complete your registration.", null));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody final LoginRequest request) throws MessagingException {
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody final LoginRequest request)
+            throws MessagingException {
         final LoginResponse response = authenticationService.login(request);
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/register-user")
-    @PreAuthorize("hasRole('INSTITUTION_ADMIN')")
-    public ResponseEntity<ApiResponse<String>> registerUser(@Valid @RequestBody final RegisterUserRequest registerUserRequest) throws MessagingException {
-        authenticationService.createUser(registerUserRequest);
-        return ResponseEntity.ok(ApiResponse.success(true, "Almost there! Check your email to complete your registration.", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "login successful", response));
     }
 
     @GetMapping("/verify-user")
-    public ResponseEntity<ApiResponse<String>> verifyUser(@RequestParam final String verificationTokenFromRequest, @RequestParam final String email) {
+    public ResponseEntity<ApiResponse<String>> verifyUser(@RequestParam final String verificationTokenFromRequest,
+                                                          @RequestParam final String email) {
         authenticationService.verifyUser(verificationTokenFromRequest, email);
         return ResponseEntity.ok(ApiResponse.success(true, "User registered successfully", null));
     }
@@ -61,29 +62,37 @@ public class AuthController {
     @GetMapping("/resend-user-verification")
     public ResponseEntity<ApiResponse<String>> resendUserVerification(@RequestParam final String email) {
         authenticationService.resendUserVerificationToken(email);
-        return ResponseEntity.ok(ApiResponse.success(true, "Resent! Check your email to complete your registration.", null));
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Resent! Check your email to complete your registration.", null));
     }
 
     @PostMapping("/refresh-token")
-    public ResponseEntity<ApiResponse<Object>> sendRefreshToken(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest){
+    public ResponseEntity<ApiResponse<Object>> sendRefreshToken(
+            @Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
         LoginResponse tokenPair = authenticationService.refreshToken(refreshTokenRequest);
-        return ResponseEntity.ok(ApiResponse.success(tokenPair));
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Refresh token sent successfully", tokenPair));
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<ApiResponse<String>> changePassword(@Valid @RequestBody ChangePasswordRequest changePasswordRequest){
+    public ResponseEntity<ApiResponse<String>> changePassword(
+            @Valid @RequestBody ChangePasswordRequest changePasswordRequest) {
         authenticationService.changePassword(changePasswordRequest);
-        return ResponseEntity.ok(ApiResponse.success(true, "Password changed successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Password changed successfully", null));
     }
 
     @PostMapping("/forgot-password")
-    public ResponseEntity<ApiResponse<String>> forgotPassword(@RequestBody ForgotPasswordRequest request) throws MessagingException {
+    public ResponseEntity<ApiResponse<String>> forgotPassword(@RequestBody ForgotPasswordRequest request)
+            throws MessagingException {
         authenticationService.forgotPassword(request);
-        return ResponseEntity.ok(ApiResponse.success(true, "Check your email to reset your password", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Check your email to reset your password",
+                null));
     }
 
     @PostMapping("/reset-password")
-    public ResponseEntity<ApiResponse<String>> resetPassword(@Valid @RequestBody ResetPasswordRequest request, @RequestParam String token) {
+    public ResponseEntity<ApiResponse<String>> resetPassword(@Valid @RequestBody ResetPasswordRequest request,
+                                                             @RequestParam String token) {
         authenticationService.resetPasswordWithToken(token, request);
         return ResponseEntity.ok(ApiResponse.success(true, "Password reset successful", null));
     }
--- src/main/java/com/bank/controllers/InstitutionAdminController.java
+++ src/main/java/com/bank/controllers/InstitutionAdminController.java
@@ -34,95 +34,110 @@ public class InstitutionAdminController {
     private final InstitutionService institutionService;
 
     @PostMapping("/register-user")
-    public ResponseEntity<ApiResponse<String>> registerUser(@Valid @RequestBody final RegisterUserRequest registerUserRequest) throws MessagingException {
+    public ResponseEntity<ApiResponse<String>> registerUser(@Valid @RequestBody RegisterUserRequest registerUserRequest)
+            throws MessagingException {
         authenticationService.createUser(registerUserRequest);
-        return ResponseEntity.ok(ApiResponse.success(true, "Almost there! Check your email to complete your registration.", null));
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Almost there! Check your email to complete your registration.", null));
     }
 
     @GetMapping("/all-users")
-    public ResponseEntity<PageResponse<UserResponse>> getAllUsers(
+    public ResponseEntity<ApiResponse<PageResponse<UserResponse>>> getAllUsers(
             @RequestParam(name = "page", defaultValue = "0") final int page,
             @RequestParam(name = "size", defaultValue = "10") final int size) {
-        return ResponseEntity.ok(institutionService.getAllUsers(page, size));
+        return ResponseEntity.ok(ApiResponse.success(true, "Users retrieved successfully",
+                institutionService.getAllUsers(page, size)));
     }
 
     @PostMapping("/create-products")
-    public ResponseEntity<ApiResponse<String>> create(@Valid @RequestBody LoanProductRequest loanProductRequest){
+    public ResponseEntity<ApiResponse<String>> create(@Valid @RequestBody LoanProductRequest loanProductRequest) {
         loanProductService.create(loanProductRequest);
-        return ResponseEntity.ok(ApiResponse.success(true, "Loan product created successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Loan product created successfully",
+                null));
     }
 
     @GetMapping("/get-single-product")
-    public ResponseEntity<ApiResponse<String>> getSingle(String id){
+    public ResponseEntity<ApiResponse<String>> getSingle(String id) {
         loanProductService.getSingle(id);
-        return ResponseEntity.ok(ApiResponse.success(true, "Loan product retrieved successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Loan product retrieved successfully",
+                null));
     }
 
     @GetMapping("/get-all-products")
-    public ResponseEntity<PageResponse<LoanProductResponse>> getAllProducts(
+    public ResponseEntity<ApiResponse<PageResponse<LoanProductResponse>>> getAllProducts(
             @RequestParam(name = "page", defaultValue = "0") final int page,
             @RequestParam(name = "size", defaultValue = "10") final int size) {
-        return ResponseEntity.ok(loanProductService.getAll(page, size));
+        return ResponseEntity.ok(ApiResponse.success(true, "Products retrieved successfully",
+                loanProductService.getAll(page, size)));
     }
 
     @PutMapping("/update-product")
-    public ResponseEntity<ApiResponse<String>> update(String id, LoanProductRequest loanProductRequest){
+    public ResponseEntity<ApiResponse<String>> update(String id, LoanProductRequest loanProductRequest) {
         loanProductService.update(id, loanProductRequest);
-        return ResponseEntity.ok(ApiResponse.success(true, "Loan product updated successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Loan product updated successfully",
+                null));
     }
 
     @GetMapping("/delete-product")
-    public ResponseEntity<ApiResponse<String>> delete(String id){
+    public ResponseEntity<ApiResponse<String>> delete(String id) {
         loanProductService.delete(id);
-        return ResponseEntity.ok(ApiResponse.success(true, "Loan product deleted successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Loan product deleted successfully",
+                null));
     }
 
     @GetMapping("/all-members")
     public ResponseEntity<ApiResponse<PageResponse<MemberResponse>>> getMembers(
             @RequestParam ProfileStatus profileStatus,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10")  int size) {
+            @RequestParam(defaultValue = "10") int size) {
         return ResponseEntity.ok(ApiResponse.success(true, "Members retrieved successfully",
                 memberService.getAllMembers(profileStatus, page, size)));
     }
 
     @PostMapping("/activate")
-    public ResponseEntity<ApiResponse<String>> activate(@RequestParam String savingsId){
+    public ResponseEntity<ApiResponse<String>> activate(@RequestParam String savingsId) {
         savingsService.activateAccount(savingsId);
-        return ResponseEntity.ok(ApiResponse.success(true, "Savings account activated successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Savings account activated successfully",
+                null));
     }
 
     @PostMapping("/freeze")
-    public ResponseEntity<ApiResponse<String>> freeze(@RequestParam String savingsId){
+    public ResponseEntity<ApiResponse<String>> freeze(@RequestParam String savingsId) {
         savingsService.freezeAccount(savingsId);
-        return ResponseEntity.ok(ApiResponse.success(true, "Savings account frozen successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Savings account frozen successfully",
+                null));
     }
 
     @PostMapping("/close")
-    public ResponseEntity<ApiResponse<String>> close(@RequestParam String savingsId){
+    public ResponseEntity<ApiResponse<String>> close(@RequestParam String savingsId) {
         savingsService.closeAccount(savingsId);
-        return ResponseEntity.ok(ApiResponse.success(true, "Savings account closed successfully", null));
+        return ResponseEntity.ok(ApiResponse.success(true, "Savings account closed successfully",
+                null));
     }
 
     @GetMapping("/total-savings")
-    public ResponseEntity<TotalSavingsResponse> getSavingsSummary() {
-        return ResponseEntity.ok(savingsService.getTotalSavings());
+    public ResponseEntity<ApiResponse<TotalSavingsResponse>> getSavingsSummary() {
+        return ResponseEntity.ok(ApiResponse.success(true, "Total savings calculated successfully",
+                savingsService.getTotalSavings()));
     }
 
     @GetMapping("/total-outstanding-loans")
-    public ResponseEntity<TotalLoansOutstandingResponse> getTotalLoansOutstanding() {
-        return ResponseEntity.ok(savingsService.getTotalLoansOutstanding());
+    public ResponseEntity<ApiResponse<TotalLoansOutstandingResponse>> getTotalLoansOutstanding() {
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Total outstanding loans calculated successfully", savingsService.getTotalLoansOutstanding()));
     }
 
     @GetMapping("/total-overdue-loans")
-    public ResponseEntity<TotalLoansOverdueResponse> getTotalLoansOverdue() {
-        return ResponseEntity.ok(savingsService.getTotalLoansOverdue());
+    public ResponseEntity<ApiResponse<TotalLoansOverdueResponse>> getTotalLoansOverdue() {
+        return ResponseEntity.ok(ApiResponse.success(true,
+                "Total overdue loans calculated successfully", savingsService.getTotalLoansOverdue()));
     }
 
     @GetMapping("/total-interest")
-    public ResponseEntity<TotalInterestCollectedResponse> getTotalInterest(
+    public ResponseEntity<ApiResponse<TotalInterestCollectedResponse>> getTotalInterest(
             @RequestParam(value = "month", required = false) final Month month,
             @RequestParam(value = "year", required = false) final Year year) {
-        return ResponseEntity.ok(savingsService.getTotalInterestCollected(month, year));
+        return ResponseEntity.ok(ApiResponse.success(true, "Total interest calculated successfully",
+                savingsService.getTotalInterestCollected(month, year)));
     }
 }
