3a3da4a84c6d804871960bd880d01b05afe930f7
macbook <graceolubiyi@gmail.com>
2026-05-27 13:28:24 +0100

    wip

--- src/main/java/com/bank/services/impl/LoanApplicationServiceImpl.java
+++ src/main/java/com/bank/services/impl/LoanApplicationServiceImpl.java
@@ -6,6 +6,7 @@ import com.bank.controllers.LoanOfficerController;
 import com.bank.entities.*;
 import com.bank.enums.LoanApplicationStatus;
 import com.bank.enums.UserAccountType;
+import com.bank.exceptions.DuplicateResourceException;
 import com.bank.exceptions.InvalidRequestException;
 import com.bank.mapper.LoanApplicationMapper;
 import com.bank.repositories.LoanApplicationRepository;
@@ -56,32 +57,32 @@ public class LoanApplicationServiceImpl implements LoanApplicationService {
         boolean approved = loanApplicationRepository.existsByMemberIdAndLoanApplicationStatus(existingMember.getId(), LoanApplicationStatus.APPROVED);
         if (approved) {
             log.debug("You have an approved loan");
-            throw new DuplicateRequestException("You have an approved loan");
+            throw new DuplicateResourceException("You have an approved loan");
         }
         boolean pending = loanApplicationRepository.existsByMemberIdAndLoanApplicationStatus(existingMember.getId(), LoanApplicationStatus.PENDING);
         if (pending) {
             log.debug("You have a pending loan application");
-            throw new DuplicateRequestException("You have a pending loan application");
+            throw new DuplicateResourceException("You have a pending loan application");
         }
         boolean defaulted = loanApplicationRepository.existsByMemberIdAndLoanApplicationStatus(existingMember.getId(), LoanApplicationStatus.DEFAULTED);
         if (defaulted) {
             log.debug("Cannot apply for a loan at the moment. You have an outstanding loan payment");
-            throw new DuplicateRequestException("Cannot apply for a loan at the moment. You have an outstanding loan payment");
+            throw new DuplicateResourceException("Cannot apply for a loan at the moment. You have an outstanding loan payment");
         }
         boolean disbursed = loanApplicationRepository.existsByMemberIdAndLoanApplicationStatus(existingMember.getId(), LoanApplicationStatus.DISBURSED);
         if (disbursed) {
             log.debug("You have an active loan. Try again after repayment");
-            throw new DuplicateRequestException("You have an active loan. Try again after repayment");
+            throw new DuplicateResourceException("You have an active loan. Try again after repayment");
         }
         boolean underReview = loanApplicationRepository.existsByMemberIdAndLoanApplicationStatus(existingMember.getId(), LoanApplicationStatus.UNDER_REVIEW);
         if (underReview) {
             log.debug("Your previous application is still under review");
-            throw new DuplicateRequestException("Your previous application is still under review");
+            throw new DuplicateResourceException("Your previous application is still under review");
         }
         boolean writtenOff = loanApplicationRepository.existsByMemberIdAndLoanApplicationStatus(existingMember.getId(), LoanApplicationStatus.WRITTEN_OFF);
         if (writtenOff) {
             log.debug("Cannot apply for a loan.");
-            throw new DuplicateRequestException("Cannot apply for a loan.");
+            throw new DuplicateResourceException("Cannot apply for a loan.");
         }
         LoanApplication loanApplication = loanApplicationMapper.toEntity(loanApplicationRequest);
         loanApplication.setInstitution(Institution.builder().id(institutionId).build());
