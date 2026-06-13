90e0aba408f5586d9fe8f957046f3693e16e21a6
macbook <graceolubiyi@gmail.com>
2026-06-14 00:21:54 +0100

    wip

--- src/main/java/com/bank/transactions/TransactionServiceImpl.java
+++ src/main/java/com/bank/transactions/TransactionServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
@@ -24,24 +25,24 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
-public class TransactionServiceImpl implements TransactionService{
+public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
     private final TransactionMapper transactionMapper;
     private final SavingsRepository savingsRepository;
-    private  final EmailService emailService;
+    private final EmailService emailService;
 
     @Override
     public void createDeposit(TransactionRequest transactionRequest) throws MessagingException {
-        if(!transactionRequest.getTransactionType().equals(TransactionType.DEPOSIT)){
+        if (!transactionRequest.getTransactionType().equals(TransactionType.DEPOSIT)) {
             throw new InvalidRequestException("only deposits are allowed");
         }
 
         SavingsAccount savingsAccount = savingsRepository.findByAccountNumber(transactionRequest.getAccountNumber())
                 .orElseThrow(() -> new InvalidRequestException("Invalid account number"));
-        if (!savingsAccount.getSavingsAccountType().equals(SavingsAccountType.REGULAR)){
-           throw new InvalidRequestException("Deposits can only be made into a regular account");
+        if (!savingsAccount.getSavingsAccountType().equals(SavingsAccountType.REGULAR)) {
+            throw new InvalidRequestException("Deposits can only be made into a regular account");
         }
-        if (!savingsAccount.getSavingsStatus().equals(SavingsStatus.ACTIVE)){
+        if (!savingsAccount.getSavingsStatus().equals(SavingsStatus.ACTIVE)) {
             throw new InvalidRequestException("Deposit can only be made into an active account");
         }
 
@@ -57,22 +58,29 @@ public class TransactionServiceImpl implements TransactionService{
         transaction.setBalanceBefore(savingsAccount.getBalance());
         savingsAccount.setBalance((savingsAccount.getBalance()).add(transactionRequest.getAmount()));
         transaction.setBalanceAfter(savingsAccount.getBalance());
-        transaction.setReference("TXN-" + LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)
+        transaction.setReference("TXN-" + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE)
                 + savingsAccount.getInstitution().getId().replace("-", "")
-                + UUID.randomUUID().toString().replace("-", ""));
+                + UUID.randomUUID().toString().substring(0, 4).toUpperCase());
         transaction.setTransactionStatus(TransactionStatus.COMPLETED);
-
         savingsRepository.save(savingsAccount);
         transactionRepository.save(transaction);
 
         Map<String, Object> model = new HashMap<>();
+
+        String accountNumber = savingsAccount.getAccountNumber();
+        String maskedAccountNumber = accountNumber.substring(0, 2)
+                + "*".repeat(accountNumber.length() - 4)
+                + accountNumber.substring(accountNumber.length() - 2);
+
+        String transactionDate = formatDateTime(transaction.getCreatedAt());
+
         model.put("name", accountHolder.getName());
         model.put("amount", CurrencyUtil.naira(transaction.getAmount()));
         model.put("newBalance", CurrencyUtil.naira(transaction.getBalanceAfter()));
         model.put("institutionName", savingsAccount.getInstitution().getInstitutionName());
-        model.put("accountNumber", savingsAccount.getAccountNumber());
+        model.put("accountNumber", maskedAccountNumber);
         model.put("transactionReference", transaction.getReference());
-        model.put("transactionDate", transaction.getCreatedAt());
+        model.put("transactionDate", transactionDate);
         model.put("currentYear", Year.now().getValue());
         model.put("institutionPhone", savingsAccount.getInstitution().getInstitutionPhone());
         model.put("institutionEmail", savingsAccount.getInstitution().getInstitutionEmail());
@@ -84,5 +92,23 @@ public class TransactionServiceImpl implements TransactionService{
                 "deposit",
                 model
         );
+    }
+
+    private String formatDateTime(LocalDateTime dateTime) {
+        int day = dateTime.getDayOfMonth();
+
+        String suffix;
+        if (day >= 11 && day <= 13) {
+            suffix = "th";
+        } else {
+            switch (day % 10) {
+                case 1 -> suffix = "st";
+                case 2 -> suffix = "nd";
+                case 3 -> suffix = "rd";
+                default -> suffix = "th";
+            }
+        }
+        return day + suffix + " "
+                + dateTime.format(DateTimeFormatter.ofPattern("MMMM, yyyy 'at' hh:mma"));
     }
 }
